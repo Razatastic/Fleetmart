4a8d7c0a2ba7db67fc3faa4c1e8e9984349ae835
Ahmad Raza <ahmadraza@ahmads-mbp.myfiosgateway.com>
2020-03-30 02:10:14 -0400

    Add axios

--- src/client/src/pages/Home.js
+++ src/client/src/pages/Home.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:5000/test_route`).then(res => {
+      console.log(res);
+    });
+  });
   return (
     <div>
       <h1>home</h1>
